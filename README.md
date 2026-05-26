227603906fef72b0fe129ae7c11a72ac1eca1db7
Jnyandeep <thokchom.jnyandeep@gmail.com>
2026-05-26 20:01:34 +0530

    feat: single threaded

---
+++ README.md
@@ -0,0 +1,10 @@
+# Multithreaded Web Crawler
+
+A rust multi threaded web crawler to download images and learn how concurrency works in rust.
+
+## Benchmarks
+
+|                 | no of images | time taken   |
+| --------------- | ------------ | ------------ |
+| Single Threaded | 20           | 17.590181459 |
+| Multi Threaded  | 20           |              |
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
+use std::{error::Error, fs::File, time::Instant};
+
 fn main() {
-    println!("Hello, world!");
+    let images = vec![
+        "https://picsum.photos/200/300?grayscale",
+        "https://picsum.photos/200/300?grayscale",
+        "https://picsum.photos/200/300?grayscale",
+        "https://picsum.photos/200/300?grayscale",
+        "https://picsum.photos/200/300?grayscale",
+        "https://picsum.photos/200/300?grayscale",
+        "https://picsum.photos/200/300?grayscale",
+        "https://picsum.photos/200/300?grayscale",
+        "https://picsum.photos/200/300?grayscale",
+        "https://picsum.photos/200/300?grayscale",
+        "https://picsum.photos/200/300?grayscale",
+        "https://picsum.photos/200/300?grayscale",
+        "https://picsum.photos/200/300?grayscale",
+        "https://picsum.photos/200/300?grayscale",
+        "https://picsum.photos/200/300?grayscale",
+        "https://picsum.photos/200/300?grayscale",
+        "https://picsum.photos/200/300?grayscale",
+        "https://picsum.photos/200/300?grayscale",
+        "https://picsum.photos/200/300?grayscale",
+        "https://picsum.photos/200/300?grayscale",
+    ];
+
+    let now = Instant::now();
+
+    for (index, url) in images.iter().enumerate() {
+        println!("Downloading image {}", index + 1);
+
+        match download_image(url, index) {
+            Ok(_) => println!("Successfully saved image as image_{}.jpg", index + 1),
+            Err(e) => eprintln!("Failed to download image {}: {}", index + 1, e),
+        }
+    }
+
+    let duration = now.elapsed();
+    println!("Took {:?}", duration);
+}
+
+fn download_image(url: &str, index: usize) -> Result<(), Box<dyn Error>> {
+    let mut response = reqwest::blocking::get(url)?;
+
+    let file_name = format!("asset/image_{}.jpg", index + 1);
+    let mut file = File::create(file_name)?;
+
+    response.copy_to(&mut file)?;
+
+    Ok(())
 }
